88dc9fd9d8ea7c977100b1cc416551c42dc9d658
danny305 <danny.jesus.diaz.94@gmail.com>
2017-12-30 21:09:11 -0600

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 
 from lists.views import home_page
 
@@ -10,20 +12,44 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+
+        #The proceeding two lines test our implementation and not constants
+        response = self.client.get('/')                         #Utilizing django Test Client tool (built in way to check template used
+        self.assertTemplateUsed(response,'wrong.html')          # This test method ONLY works on responses retrieved by the test client
 
 
 
+        # html = response.content.decode('utf8')                  #We can delete this and all the 3 proceeding tests bc the
+        # self.assertTrue(html.startswith('<!DOCTYPE html>'))     #assertTemplateUsed test confirms if we are using the right template
+        # self.assertIn('<title>To-Do lists</title>',html)
+        # self.assertTrue(html.strip().endswith('</html>'))
+
+
+
+
+        # request = HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf8')
+        # expected_html = render_to_string('home.html')   #12-30-17 8:25 pm    new way to manually render the template
+        # self.assertEqual(html,expected_html)            #ourselves in the testand then compare that to what the view returns.
+                                                        #(Use django's functionrender_to_string that allows us to do this
+
+
+
+
+        # print(repr(html))                      #checking to see why the startswith(<html>) did not work turn out its <!DOCTYPE html>.
+        # self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        # self.assertIn('<title>To-Do lists</title>',html)
+        # self.assertTrue(html.strip().endswith('</html>'))
+
+
+
+
+        # def test_root_url_resolves_to_home_page_view(self):       #Was the first test written but got supplanted by the
+        #     found = resolve('/')                                  #test_home_page_returns_correct_html func bc the
+        #     self.assertEqual(found.func,home_page)                #Test Client implicitly tests for the resolve of the root url
 
 
 #12-30-17 2:20 pm
@@ -34,3 +60,7 @@ class HomePageTest(TestCase):
 #         self.assertEqual(1 + 1, 3)
 
 
+
+
+if __name__=='__main__':
+    print (HomePageTest().test_home_page_returns_correct_html())
