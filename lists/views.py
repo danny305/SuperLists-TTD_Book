6ca21de8cdfb325056c6c567458a5fe9c92ed749
danny305 <danny.jesus.diaz.94@gmail.com>
2018-01-24 15:05:39 -0600

    new URL, view and template to display list

--- lists/views.py
+++ lists/views.py
@@ -11,10 +11,10 @@ from django.http import HttpResponse
 def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 
 
@@ -43,6 +43,13 @@ def home_page(request):
     # return render(request, 'home.html', {
     #     'new_item_text': request.POST.get('item_text',''),
     # })
+
+
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
 
 
 def main_page(request):
